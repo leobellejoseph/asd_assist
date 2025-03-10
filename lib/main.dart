5416eb49a66b9c6c3e379e20067a909bbae13830
Leobelle Joseph <leobellejoseph@outlook.com>
2025-03-10 14:43:21 +0800

    initial push

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false);
   }
 }
 
